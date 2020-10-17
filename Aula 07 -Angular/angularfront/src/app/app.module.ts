02863ae145c7a1720bacceaff29e0aea8b72b88c
bmaru <57016516+bmaru@users.noreply.github.com>
2020-10-17 17:35:54 -0300

    novos projetos

--- Aula 07 -Angular/angularfront/src/app/app.module.ts
+++ Aula 07 -Angular/angularfront/src/app/app.module.ts
@@ -8,6 +8,14 @@ import { PostDetailsComponent } from './components/post-details/post-details.com
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -20,7 +28,14 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
